98fc6f1ddd180178dc222689b1055a0c6b5edb1f
scoontech <scoon@revereschools.org>
2026-02-04 07:35:04 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener('fetch', (event) => {
     // This allows the app to work offline if you add caching later
 });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(() => console.log("Service Worker Registered"));
+}
